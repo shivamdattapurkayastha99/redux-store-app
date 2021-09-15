ec0ec3e0888f34b99cec813448e8d7b0ebf31edf
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-09-15 22:15:19 +0530

    first commit

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Buy shoes for Rs 100</h2>
+            <button className="btn btn-primary mx-2">-</button>
+            Add to cart
+            <button className="btn btn-primary mx-2">+</button>
+            {/* Remove From cart */}
+        </div>
+    )
+}
+
+export default Shop
