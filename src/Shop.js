a1cb230e64dee5434e20b94af3936f3dd902fe7f
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-10-05 23:59:39 +0530

    second commit

--- src/Shop.js
+++ src/Shop.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from './state/index'
+import { useSelector } from 'react-redux'
 
 const Shop = () => {
+    const dispatch = useDispatch()
+    const {withdrawMoney,depositMoney}=bindActionCreators(actionCreators,dispatch);
+    const balance = useSelector(state => state.amount)
     return (
         <div>
-            <h2>Deposit/Withdraw Money </h2>
-            <button className="btn btn-primary mx-2">-</button>
+            {/* <h2>Deposit/Withdraw Money </h2>
+            <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
             Update Balance 
-            <button className="btn btn-primary mx-2">+</button>
+            <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button> */}
             {/* Remove From cart */}
+            <h2>Deposit/Withdraw Money </h2>
+            <button className="btn btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
+            Update Balance {balance}
+            <button className="btn btn-primary mx-2" onClick={()=>{depositMoney(100)}}>+</button>
         </div>
     )
 }
